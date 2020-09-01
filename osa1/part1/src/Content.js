0ff73c7127d2aa03935ec29484ff0091c1765c5b
Mira Hiltunen <mira.hiltunen@outlook.com>
2020-09-01 18:35:21 +0300

    1.3: kurssitiedot step3

--- osa1/part1/src/Content.js
+++ osa1/part1/src/Content.js
@@ -4,9 +4,9 @@ import Part from './Part'
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
   )
 }
