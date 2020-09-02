9bf94fed3f1065b33a9225678debe2f8697be3e8
Mira Hiltunen <mira.hiltunen@outlook.com>
2020-09-02 12:16:22 +0300

    1.6: unicafe step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // Lets make a function return another function because why not
+  const giveFeedback = (feedback) => {
+    let increaseFeedback
+
+    if (feedback === 'good') {
+      increaseFeedback = () => setGood(good + 1)
+    } else if (feedback === 'neutral') {
+      increaseFeedback = () => setNeutral(neutral + 1)
+    } else {
+      increaseFeedback = () => setBad(bad + 1)
+    }
+
+    return increaseFeedback
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={giveFeedback('good')}>good</button>
+      <button onClick={giveFeedback('neutral')}>neutral</button>
+      <button onClick={giveFeedback('bad')}>bad</button>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
