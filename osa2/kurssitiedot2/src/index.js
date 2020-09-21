13102ac20071557cd394c5aa081d060bd5b177c4
Mira Hiltunen <mira.hiltunen@outlook.com>
2020-09-21 19:57:01 +0300

    2.4: kurssitiedot step9, 2.5: erillinen moduuli

--- osa2/kurssitiedot2/src/index.js
+++ osa2/kurssitiedot2/src/index.js
@@ -3,36 +3,49 @@ import ReactDOM from 'react-dom'
 import Course from "./Course"
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'New course with plenty of exercises!',
-        exercises: 100,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'New course with plenty of exercises!',
+          exercises: 100,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'One more course',
+      id: 2,
+      parts: [
+        {
+          name: 'Second course, not much content here',
+          exercises: 1,
+          id: 1
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => <Course key={course.id} course={course} /> )}
     </div>
   )
 }
