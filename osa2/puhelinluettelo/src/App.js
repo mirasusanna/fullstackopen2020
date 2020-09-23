21d148dc525901fd0ccdb9434d85a3d701d7a2f8
Mira Hiltunen <mira.hiltunen@outlook.com>
2020-09-23 15:25:30 +0300

    2.6: puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Person from './Person'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = event => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = event => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+       {persons.map(person => <Person key={person.name} name={person.name} /> )}
+    </div>
+  )
+}
+
+export default App
